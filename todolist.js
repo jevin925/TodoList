aa81d39bb40bc16e0adbffab963c2562b8e1e896
Jevin Vekaria <jevin925@gmail.com>
2020-07-19 15:14:37 +0530

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,26 @@
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+});
+
+
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+
+
+$("input[type='text']").keypress(function(event){
+	if(event.which=== 13){
+		var todotext= $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span> " +  todotext + " </li>");	}
+});
+
+$(".fa-plus-square").click(function(){
+	$("input[type='text']").fadeToggle(200);
+	
+});
